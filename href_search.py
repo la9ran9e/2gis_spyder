c32ab427e4788075620402c586fadfbcbe4cddc2
la9ran9e.laptop <tvauritimur@gmail.com>
2017-04-16 03:53:07 +0300

    href_search.py refactored

--- href_search.py
+++ href_search.py
@@ -6,9 +6,6 @@ from selenium.common.exceptions import (NoSuchElementException, ElementNotVisibl
                                         StaleElementReferenceException, NoSuchWindowException)
 from bs4 import BeautifulSoup
 PATH = os.getcwd()
-browser = webdriver.PhantomJS('%s/phantomjs' % PATH)
-#Chrome('/home/la9ran9e/chromedriver')
-#PhantomJS('%s/phantomjs' % PATH)
 BLOCK_class = 'searchResults__list'
 next_page_class = 'pagination__arrow _right'
 next_page_xpath = '/html/body/div[1]/div/div[1]/div[2]\
@@ -17,64 +14,48 @@ div/div[2]/div/div[4]/div[2]/div[2]'
 RESULTS_xpath = '/html/body/div[1]/div/div[1]\
 /div[2]/div[2]/div[3]/div/div[3]/div/div[3]/div[2]/div/div/div/div/div[1]/h2'
 RESULTS_class = 'searchResults__headerName'
-URL ='https://2gis.ru/moscow/search/\
+testUrl_0 = 'https://2gis.ru/moscow/search/\
 %D0%AD%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%8B%\
 D0%B5%20%D1%81%D0%B8%D0%B3%D0%B0%D1%80%D0%B5%D1%82%D1%8B%20%2F%20%D0%BA%D0%B0\
 %D0%BB%D1%8C%D1%8F%D0%BD%D1%8B/tab/firms?queryState=zoom%2F11'
-'''https://2gis.ru/moscow/search/adidas\
-%20%D0%B4%D0%B8%D1%81%D0%BA%D0%BE%D0%BD%D1%82/tab/firms?queryState=zoom%2F11'''
-'''https://2gis.ru/moscow/search/\
-%D0%AD%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%8B%\
-D0%B5%20%D1%81%D0%B8%D0%B3%D0%B0%D1%80%D0%B5%D1%82%D1%8B%20%2F%20%D0%BA%D0%B0\
-%D0%BB%D1%8C%D1%8F%D0%BD%D1%8B/tab/firms?queryState=zoom%2F11'''
+testUrl_1 = 'https://2gis.ru/moscow/search/adidas\
+%20%D0%B4%D0%B8%D1%81%D0%BA%D0%BE%D0%BD%D1%82/tab/firms?queryState=zoom%2F11'
+URL = testUrl_0
 id_ = re.compile(r'id="[a-z0-9\-]*"')
-link = re.compile(r'\?http[\:\/\.a-zA-Z0-9\-\_\+]*$')
+link = re.compile(r'\?http.*$')
 nums = re.compile(r'^[0-9]+')
-browser.get(URL)
-time.sleep(3)
-RESULTS_elem = browser.find_element_by_class_name(RESULTS_class)
-RESULTS_ = RESULTS_elem.text
-RESULTS = nums.search(RESULTS_)
-RESULTS = RESULTS.group(0)
-print(RESULTS)
-list_dir = os.listdir(PATH)
-if 'log' in list_dir:
-    log_file = open('%s/log' % PATH)
-    log_line = log_file.read()
-    log = log_line.split(';')
-else:
-    log = []
-if '2gis_base' in list_dir:
-    w = open('%s/2gis_base' % PATH, 'a')
-else:
-    w = open('%s/2gis_base' % PATH, 'w')
-    w.write(';'.join(['name', 'address', 'site', 'vk.com',
-             'twitter.com', 'facebook.com', 'instagram.com',
-                      'youtube.com', 'plus.google.com', 'telegram', 'ok.ru',
-                      'tel', 'skype'])\
-            +'\n')
-         
 
-def obj_call(id_name):
+def obj_call(name,
+             by = 'id',
+             m = 'repeat obj_call',
+             Exception_ = NoSuchElementException,
+             sleep = 2):
     obj = None
     while not obj:
         try:
-            obj = browser.find_element_by_id(id_name)
-        except NoSuchElementException:
-            print('repeat find obj')
-            time.sleep(2)
-    click_handler(obj)
+            if by == 'id':
+                obj = browser.find_element_by_id(name)
+            elif by == 'class':
+                obj = browser.find_element_by_class_name(name)
+        except Exception_:
+            print(m)
+            time.sleep(sleep)
+    return obj
             
-def click_handler(obj):
+def click_handler(obj,
+                  m = 'repeat click_handler',
+                  Exception_ = (ElementNotVisibleException, StaleElementReferenceException),
+                  sleep_0 = 2,
+                  sleep_1 = 5):
     Clicked = False
     while not Clicked:
         try:
             obj.click()
-            time.sleep(3)
+            time.sleep(sleep_0)
             Clicked = True
-        except (ElementNotVisibleException, StaleElementReferenceException):
-            print('repeat click')
-            time.sleep(5)
+        except Exception_:
+            print(m)
+            time.sleep(sleep_1)
             
 def search_contacts(class_name = 'contact'):
     contacts_BLOCK = None
@@ -84,23 +65,32 @@ def search_contacts(class_name = 'contact'):
         try:
             contacts_BLOCK = browser.find_element_by_class_name(class_name)
         except NoSuchElementException:
+            print('repeat search_contacts')
             try:
                 contacts_BLOCK = browser.find_element_by_class_name('mediaContacts__block')
             except NoSuchElementException:
-                time.sleep(3)        
+                time.sleep(3)
+                
+    if not contacts_BLOCK:
+        print('contacts not found')
+        
     if contacts_BLOCK:
         contacts_BLOCK_html = contacts_BLOCK.get_attribute('innerHTML')
         soup = BeautifulSoup(contacts_BLOCK_html, 'html.parser')
         elems = soup.find_all('a')
+
+        Site_found = False
         for elem in elems:
             href = elem.get('href')
-            if 'http://link.2gis.ru' in href:
+            if 'http://link.2gis.ru' in href and not Site_found :
                 l = link.search(href)
                 if l:
-                    if '//m.' not in href:
-                        href = l.group(0)[1:]
-                        contacts_dict['site'] = href
-                        write_list[2] = href
+                    if '//m.' in href:
+                        href = href.replace('m.', '')
+                    href = l.group(0)[1:]
+                    contacts_dict['site'] = href
+                    write_list[2] = href
+                    Site_found == True
             elif 'vk.com' in href:
                 contacts_dict['vk.com'] = href
                 write_list[3] = href
@@ -140,13 +130,7 @@ def search_contacts(class_name = 'contact'):
 def searcher(BLOCK_class):
     global elem, contacts_dict, write_list, iteration, log
     contacts_dict = {}
-    BLOCK = None
-    while not BLOCK:
-        try:
-            BLOCK = browser.find_element_by_class_name(BLOCK_class)
-        except NoSuchElementException:
-            print('repeat find BLOCK')
-            time.sleep(3)
+    BLOCK = obj_call(BLOCK_class, by = 'class', m = 'repeat searcher')
     BLOCK_html = BLOCK.get_attribute('innerHTML')
     soup = BeautifulSoup(BLOCK_html, 'html.parser')
     elems = soup.find_all('article')
@@ -166,8 +150,9 @@ def searcher(BLOCK_class):
             elem = str(elem)
             m = id_.search(elem)
             id_name = m.group(0)[4:-1]
-            print(id_name)
-            obj_call(id_name)
+            #print(id_name)
+            obj = obj_call(id_name)
+            click_handler(obj, sleep_0 = 1)
             search_contacts()
             log.append(name+address)
     
@@ -179,13 +164,50 @@ def find_text(class_name):
     return text
     
 def next_pages_searcher(m = 'Pages not found'):
-    try:
-        next_page = browser.find_element_by_xpath(next_page_xpath)
-        time.sleep(2)
-    except NoSuchElementException:
-        print('# %s' % m)
-        next_page = None
+    next_page = None
+    while not next_page:
+        try:
+            next_page = browser.find_element_by_xpath(next_page_xpath)
+            time.sleep(1)
+        except NoSuchElementException:
+            print('repeat next_page_searcher')
+            time.sleep(3)
     return next_page
+
+browser = webdriver.PhantomJS('%s/phantomjs' % PATH)
+#Chrome('/home/la9ran9e/chromedriver')
+#PhantomJS('%s/phantomjs' % PATH)
+time.sleep(3)
+Get_url = False
+while not Get_url:
+    try:
+        browser.get(URL)
+        Get_url = True
+    except:
+        print('repeat get_url')
+        time.sleep(3)
+
+RESULTS_elem = obj_call(RESULTS_class, by = 'class', m = 'repeat RESULT_elem')
+RESULTS_ = RESULTS_elem.text
+RESULTS = nums.search(RESULTS_)
+RESULTS = RESULTS.group(0)
+print(RESULTS)
+list_dir = os.listdir(PATH)
+if 'log' in list_dir:
+    log_file = open('%s/log' % PATH)
+    log_line = log_file.read()
+    log = log_line.split(';')
+else:
+    log = []
+if '2gis_base' in list_dir:
+    w = open('%s/2gis_base' % PATH, 'a')
+else:
+    w = open('%s/2gis_base' % PATH, 'w')
+    w.write(';'.join(['name', 'address', 'site', 'vk.com',
+             'twitter.com', 'facebook.com', 'instagram.com',
+                      'youtube.com', 'plus.google.com', 'telegram', 'ok.ru',
+                      'tel', 'skype'])\
+            +'\n')
 
 iteration = 0        
 try:
@@ -199,11 +221,12 @@ try:
                 
     else:
         searcher(BLOCK_class)
+    browser.close()
+    w.close()
+    print('Done')
 except KeyboardInterrupt:
+    print('Interrupted')
     w.close()
     log_file = open('%s/log' % PATH, 'w')
     log_file.write(';'.join(log))
     log_file.close()
-    browser.close()
-browser.close()
-w.close()
